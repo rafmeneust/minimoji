96280dd7b5cde9a00c2390550f4dd1300256c57a
rafmeneust <meneust.r@gmail.com>
2025-04-21 15:47:38 +0200

    Setup fonctionnel : Vite + React + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind + React!
+      </h1>
+    </div>
+  )
+}
+export default App
